9a67c89e87a249d8e60d05373365c80c582a13ec
jaredee-byui <jaredee@byui.edu>
2021-11-04 17:44:30 -0600

    Updated director.py to include the game loop method and several other changes.

--- 07-speed/speed/game/director.py
+++ 07-speed/speed/game/director.py
@@ -22,5 +22,37 @@ class Director:
         self._snake = Buffer()
 
     def start_game(self):
-        #todo: fill this in
+        '''
+        starts the game loop to control the sequence of play.
+
+        args:
+            self (director): an instance of Director.
+        '''
+        print("Launching Speed....")
+        self._output_service.open_window("Speed")
+
+        while self._keep_playing:
+            self._get_inputs()
+            self._do_updates()
+            self._do_outputs()
+
+            if self._input_service.window_should_close():
+                self._keep_playing = False
+
+        print("Game end!")
+
+    def _get_inputs(self):
         pass
+
+    def _do_updates(self):
+        pass
+
+    def _do_outputs(self):
+        pass
+
+    def _populate_words(self):
+        pass
+
+    def _update_words(self):
+        pass
+        
