7844c4f4d6091b0bee13d02385ffb4697454c3b2
Sean Compton <seancompton@byui.edu>
2021-11-03 18:37:19 -0600

    it's ready to go

--- 07-speed/speed/game/coordinate_point.py
+++ 07-speed/speed/game/coordinate_point.py
@@ -1,4 +1,11 @@
 class CoordinatePoints:
 
-    def __init__(self):
-        pass
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y 
+
+    def get_x(self):
+        return self._x
+
+    def get_y(self):
+        return self._y
