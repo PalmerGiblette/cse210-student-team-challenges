f2eb8bae193ae6dde604ce76a7cb035dab558acb
Sean Compton <seancompton@byui.edu>
2021-11-04 18:07:12 -0600

    I modified output services to be able to display everything

--- 07-speed/speed/game/output_service.py
+++ 07-speed/speed/game/output_service.py
@@ -15,3 +15,35 @@ class OutputService:
         '''
         raylibpy.init_window(constants.MAX_X, constants.MAX_Y, title)
         raylibpy.set_target_fps(constants.FRAME_RATE)
+
+    def draw_actor(self, actor):
+        """Renders the given actor's text on the screen.
+
+        Args:
+            self (OutputService): An instance of OutputService.
+            actor (Actor): The actor to render.
+        """ 
+        text = actor.get_text()
+        position = actor.get_position()
+        x = position.get_x()
+        y = position.get_y()
+
+        raylibpy.draw_text(text, x, y, 16, raylibpy.BLUE)
+
+    def draw_actors(self, actors):
+        """Renders the given list of actors on the screen.
+
+        Args:
+            self (OutputService): An instance of OutputService.
+            actors (list): The actors to render.
+        """ 
+        for actor in actors:
+            self.draw_actor(actor)
+    
+    def flush_buffer(self):
+        """Renders the screen.
+
+        Args:
+            self (OutputService): An instance of OutputService.
+        """ 
+        raylibpy.end_drawing()
